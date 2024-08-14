c26d0b34f1af85c9c9d7837eff993c807a52d448
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-08-14 17:46:11 +0530

    Added whatsapp

--- src/pages/Send.jsx
+++ src/pages/Send.jsx
@@ -10,7 +10,11 @@ import { db } from "../firebaseconfig";
 const Send = () => {
     const { id } = useParams();
     const [loading, setLoading] = useState(false);
-
+    const [open, setOpen] = useState(true);
+    const [title, setTitle] = useState("");
+    const [popUpmessage, setPopUpMessage] = useState("");
+    const [link, setLink] = useState(null);
+    ("Your request has been sent successfully. We will get back to you soon.");
     const handleSend = async (type) => {
         setLoading(true);
 
@@ -28,10 +32,15 @@ const Send = () => {
                 : 0;
             const currentTime = Date.now();
             const timeDifference =
-                (currentTime - lastRequestTime) / (1000 * 60 * 60 * 24); // Time in days
+                (currentTime - lastRequestTime) / (1000 * 60 * 60 * 12); // 12 hours
 
             if (timeDifference < 1) {
-                toast.error("You can only send a request everyday.");
+                setOpen(true);
+                setTitle("Request Limit Exceeded");
+                setPopUpMessage(
+                    "We are already processing your request made within the past 12 hours. Please try again later. For any other assistance feel free to reach out to us via whatsapp.",
+                );
+                setLink("https://wa.link/bfcxn5");
                 setLoading(false);
                 return;
             }
@@ -69,8 +78,11 @@ const Send = () => {
             await updateDoc(docRef, {
                 lastRequestTime: new Date(),
             });
-
-            toast.success("Request sent successfully");
+            setOpen(true);
+            setTitle("Request Sent");
+            setPopUpMessage(
+                "Your request has been sent successfully. You will receive a callback shortly.",
+            );
         } catch (e) {
             console.error(
                 "Error sending request: ",
@@ -83,35 +95,76 @@ const Send = () => {
     };
 
     return (
-        <div className="flex min-h-screen bg-indigo-700 px-10 py-5">
-            <div className="m-auto flex w-full max-w-xl flex-col items-center justify-center gap-5 rounded-lg bg-indigo-100 p-8 shadow-lg">
-                <img src={logo} className="w-1/2" alt="" />
+        <>
+            {open && (
+                <Popup
+                    open={open}
+                    setOpen={setOpen}
+                    message={popUpmessage}
+                    title={title}
+                    link={link}
+                />
+            )}
+            <div className="flex min-h-screen bg-indigo-700 px-10 py-5">
+                <div className="m-auto flex w-full max-w-xl flex-col items-center justify-center gap-5 rounded-lg bg-indigo-100 p-8 shadow-lg">
+                    <img src={logo} className="w-1/2" alt="" />
 
-                <button
-                    className="mt-4 w-full rounded-lg bg-red-700 px-4 py-2 text-white"
-                    onClick={() => handleSend("compliant")}
-                    disabled={loading}
-                >
-                    {loading ? (
-                        <CSpinner size="sm" color="white" />
-                    ) : (
-                        "Log Complaint"
+                    <button
+                        className="mt-4 w-full rounded-lg bg-red-700 px-4 py-2 text-white"
+                        onClick={() => handleSend("compliant")}
+                        disabled={loading}
+                    >
+                        {loading ? (
+                            <CSpinner size="sm" color="white" />
+                        ) : (
+                            "Log Complaint"
+                        )}
+                    </button>
+                    <button
+                        className="mt-4 w-full rounded-lg bg-indigo-700 px-4 py-2 text-white"
+                        onClick={() => handleSend("inventory")}
+                        disabled={loading}
+                    >
+                        {loading ? (
+                            <CSpinner size="sm" color="white" />
+                        ) : (
+                            "Request Inventory"
+                        )}
+                    </button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+const Popup = ({ open, setOpen, title, message, link }) => {
+    return (
+        <div
+            className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ${
+                open ? "" : "hidden"
+            }`}
+        >
+            <div className="w-1/3 rounded-lg bg-white p-5">
+                <h1 className="text-xl font-bold">{title}</h1>
+                <p className="text-lg">{message}</p>
+                <div className="flex justify-center gap-5">
+                    <button
+                        className="mt-4 w-full rounded-lg bg-red-700 px-4 py-2 text-white"
+                        onClick={() => setOpen(false)}
+                    >
+                        Close
+                    </button>
+                    {link && (
+                        <button
+                            className="mt-4 w-full rounded-lg bg-green-700 px-4 py-2 text-white"
+                            onClick={() => window.open(link)}
+                        >
+                            Whatsapp Us
+                        </button>
                     )}
-                </button>
-                <button
-                    className="mt-4 w-full rounded-lg bg-indigo-700 px-4 py-2 text-white"
-                    onClick={() => handleSend("inventory")}
-                    disabled={loading}
-                >
-                    {loading ? (
-                        <CSpinner size="sm" color="white" />
-                    ) : (
-                        "Request Inventory"
-                    )}
-                </button>
+                </div>
             </div>
         </div>
     );
 };
-
 export default Send;
